cce160b94187efacaa422c7b9cc2a3624395d84a
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-16 18:00:35 -0300

    commit 3

---
+++ php-reativo/io-nb.php
@@ -0,0 +1,26 @@
+<?php
+
+$streamList = [
+  fopen('arquivo.txt', 'r'),
+  fopen('arquivo2.txt', 'r'),
+];
+
+foreach ($streamList as $stream) {
+  stream_set_blocking($stream, false);
+}
+
+do {
+  $copyReadStream = $streamList;
+  $numeroDeStreams = stream_select($copyReadStream, $write, $except, 0, 200000);
+
+  if ($numeroDeStreams === 0) {
+    continue;
+  }
+
+  foreach ($copyReadStream as $key => $stream) {
+    echo fgets($stream) . PHP_EOL;
+    unset($streamList[$key]);
+  }
+} while (!empty($streamList));
+
+echo 'Li todos os arquivos' . PHP_EOL;
